5b3828cc1da46355dfdcace8665cdda6e77967f6
Tesha R <tesha@ldrdesigns.com>
2022-10-07 18:10:52 -0700

    Add styling to quiz page

--- src/App.jsx
+++ src/App.jsx
@@ -12,27 +12,41 @@ function App() {
 
   //console.log(quizData);
 
-  //map over quiz data to get questions
+  function handleClick(event) {
+    console.log('answer clicked', event.target);
+  }
 
+  //map over quiz data to get questions
   const quizQuestions = quizData.map((quizItem) => {
     return (
       <div className="question-el">
         <h2 className="question-item">{quizItem.question}</h2>
-        <ul className="answer-item">
-          <li>{quizItem.correct_answer}</li>
-          <li>{quizItem.incorrect_answers[0]}</li>
-          <li>{quizItem.incorrect_answers[1]}</li>
-          <li>{quizItem.incorrect_answers[2]}</li>
+        <ul className="answers-el">
+          <li className="answers-item" onClick={handleClick}>
+            {quizItem.correct_answer}
+          </li>
+          <li className="answers-item" onClick={handleClick}>
+            {quizItem.incorrect_answers[0]}
+          </li>
+          <li className="answers-item" onClick={handleClick}>
+            {quizItem.incorrect_answers[1]}
+          </li>
+          <li className="answers-item" onClick={handleClick}>
+            {quizItem.incorrect_answers[2]}
+          </li>
         </ul>
       </div>
     );
   });
 
   return (
-    <div className="quiz-el">
-      {quizQuestions}
-      <button className="quiz-submit-btn">Check Answers</button>
-    </div>
+    <>
+      <h1 className="page-title">Trivia Time</h1>
+      <div className="quiz-el">
+        {quizQuestions}
+        <button className="quiz-submit-btn">Check Answers</button>
+      </div>
+    </>
   );
 }
 
