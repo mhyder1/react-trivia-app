3c8e2660fb0b5860a50645fa89e54e826c0b8946
Tesha R <tesha@ldrdesigns.com>
2022-10-15 14:24:39 -0700

    Set up radio buttons for quiz form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 function App() {
   // create state to hold my quiz data
   const [quizData, setQuizData] = useState([]);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     fetch('data.json')
@@ -10,41 +11,96 @@ function App() {
       .then((data) => setQuizData(data.results));
   }, []);
 
-  //console.log(quizData);
-
-  function handleClick(event) {
-    console.log('answer clicked', event.target);
+  function handleChange(event) {
+    const { name, value, dataset } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: {
+          answer: value,
+          data: dataset.answer,
+        },
+      };
+    });
   }
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+  // className="answer-item"
   //map over quiz data to get questions
-  const quizQuestions = quizData.map((quizItem) => {
+  const quizQuestions = quizData.map((quizItem, index) => {
     return (
-      <div className="question-el">
-        <h2 className="question-item">{quizItem.question}</h2>
-        <ul className="answers-el">
-          <li className="answers-item" onClick={handleClick}>
+      <div class="question-el">
+        <h3>{quizItem.question}</h3>
+        <div className="answer-item">
+          <input
+            id={quizItem.correct_answer}
+            data-answer="right"
+            type="radio"
+            name={index}
+            value={quizItem.correct_answer}
+            onChange={handleChange}
+          />
+          <label htmlFor={quizItem.correct_answer}>
             {quizItem.correct_answer}
-          </li>
-          <li className="answers-item" onClick={handleClick}>
+          </label>
+        </div>
+        <div className="answer-item">
+          <input
+            id={quizItem.incorrect_answers[0]}
+            data-answer="wrong"
+            type="radio"
+            name={index}
+            value={quizItem.incorrect_answers[0]}
+            onChange={handleChange}
+          />
+          <label htmlFor={quizItem.incorrect_answers[0]}>
             {quizItem.incorrect_answers[0]}
-          </li>
-          <li className="answers-item" onClick={handleClick}>
+          </label>
+        </div>
+        <div className="answer-item">
+          <input
+            id={quizItem.incorrect_answers[1]}
+            className="answer-item"
+            data-answer="wrong"
+            type="radio"
+            name={index}
+            value={quizItem.incorrect_answers[1]}
+            onChange={handleChange}
+          />
+          <label htmlFor={quizItem.incorrect_answers[1]}>
             {quizItem.incorrect_answers[1]}
-          </li>
-          <li className="answers-item" onClick={handleClick}>
+          </label>
+        </div>
+        <div className="answer-item">
+          <input
+            id={quizItem.incorrect_answers[2]}
+            className="answer-item"
+            data-answer="wrong"
+            type="radio"
+            name={index}
+            value={quizItem.incorrect_answers[2]}
+            onChange={handleChange}
+          />
+          <label htmlFor={quizItem.incorrect_answers[2]}>
             {quizItem.incorrect_answers[2]}
-          </li>
-        </ul>
+          </label>
+        </div>
       </div>
     );
   });
-
+  //console.log(quizQuestions);
   return (
     <>
       <h1 className="page-title">Trivia Time</h1>
       <div className="quiz-el">
-        {quizQuestions}
-        <button className="quiz-submit-btn">Check Answers</button>
+        <form>
+          {quizQuestions}
+          <button type="submit" className="quiz-submit-btn">
+            Check Answers
+          </button>
+        </form>
       </div>
     </>
   );
