8cb6576b4bba0652ffa245f4138021e031d1e15f
Muhammad Abdusamad <muhammadabdusamad@yahoo.com>
2022-10-17 17:17:23 +0000

    working verion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
+import Inputs from './Inputs'
 // API - https://opentdb.com/api.php?amount=5&category=11&type=multiple
 function App() {
   // create state to hold my quiz data
   const [quizData, setQuizData] = useState([]);
   const [score, setScore] = useState(0);
-  const [formData, setFormData] = useState();
   const [submitted, setSubmitted] = useState(false);
+  const [selected, setSelected] = useState({})
 
   useEffect(() => {
     fetch('data.json')
@@ -13,86 +14,25 @@ function App() {
       .then((data) => setQuizData(data.results));
   }, []);
 
-  function handleChange(event) {
-    const { name, value, dataset } = event.target;
-    setFormData((prevFormData) => {
-      // Set score if answer is true
-      if (dataset.answer === 'true') {
-        setScore(score + 1);
-      }
-      return {
-        ...prevFormData,
-        [name]: {
-          answer: value,
-          data: dataset.answer,
-        },
-      };
-    });
-  }
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('formData', formData);
     setSubmitted(true);
   }
-  //map over quiz data to get questions
+
   const quizQuestions = quizData.map((quizItem, index) => {
     return (
-      <div className="question-el">
+      <div className="question-el" key={index}>
         <h3 className="question-item">{quizItem.question}</h3>
-        <div className="answer-item">
-          <input
-            id={quizItem.correct_answer}
-            data-answer={true}
-            type="radio"
-            name={index}
-            value={quizItem.correct_answer}
-            onChange={handleChange}
+          <Inputs 
+            incorrect={quizItem.incorrect_answers}
+            correct={quizItem.correct_answer}
+            index={index}
+            submitted={submitted}
+            selected={selected}
+            setSelected={setSelected}
+            score={score}
+            setScore={setScore}
           />
-          <label htmlFor={quizItem.correct_answer}>
-            {quizItem.correct_answer}
-          </label>
-        </div>
-        <div className="answer-item">
-          <input
-            id={quizItem.incorrect_answers[0]}
-            data-answer={false}
-            type="radio"
-            name={index}
-            value={quizItem.incorrect_answers[0]}
-            onChange={handleChange}
-          />
-          <label htmlFor={quizItem.incorrect_answers[0]}>
-            {quizItem.incorrect_answers[0]}
-          </label>
-        </div>
-        <div className="answer-item">
-          <input
-            id={quizItem.incorrect_answers[1]}
-            className="answer-item"
-            data-answer={false}
-            type="radio"
-            name={index}
-            value={quizItem.incorrect_answers[1]}
-            onChange={handleChange}
-          />
-          <label htmlFor={quizItem.incorrect_answers[1]}>
-            {quizItem.incorrect_answers[1]}
-          </label>
-        </div>
-        <div className="answer-item">
-          <input
-            id={quizItem.incorrect_answers[2]}
-            className="answer-item"
-            data-answer={false}
-            type="radio"
-            name={index}
-            value={quizItem.incorrect_answers[2]}
-            onChange={handleChange}
-          />
-          <label htmlFor={quizItem.incorrect_answers[2]}>
-            {quizItem.incorrect_answers[2]}
-          </label>
-        </div>
       </div>
     );
   });
